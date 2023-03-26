5a20ba41cb846e5ed4bac4d016b0757cab96df6a
Aditya <adityarags@gmail.com>
2023-03-26 15:57:04 +0530

    Data Collection Web App: Lake Selection Update

--- src/results/Data Collection Web App/application/controllers.py
+++ src/results/Data Collection Web App/application/controllers.py
@@ -12,25 +12,25 @@ credentials = ee.ServiceAccountCredentials(service_account, os.getenv("auth_pass
 ee.Initialize(credentials)
 
 
-
-geometry = ee.Geometry.Polygon([[77.39546956268856,23.265850898429633],[77.39576192347118,23.263643049175467],[77.39595504252026,23.263781040825478],[77.39609988180706,23.263738453582683],[77.39656926838467,23.26489905664098],[77.39658804384777,23.265207070617443],[77.39640297142574,23.265850201505117],[77.39614011494228,23.266249384632868],[77.3959443136842,23.26634548409614],[77.39546956268856,23.265850898429633]])
-
-
 @app.route("/errorPage")
 def errorPage():
 	return render_template("404.html")
 
 @app.route("/", methods = ["GET", "POST"])
 def home():
-	selected_val = ""
+	selected_val = selected_lake = ""
 	if request.method == "POST":
+		selected_lake = request.form["lake"]
 		selected_val = request.form["param"]
-	return render_template("index.html", selected_val = selected_val)
+	return render_template("index.html", selected_val = selected_val, selected_lake = selected_lake)
 
-@app.route("/fetch/<param>", methods = ["GET", "POST"])
-def fetch(param):
+@app.route("/fetch/<param>/<lake>", methods = ["GET", "POST"])
+def fetch(param, lake):
 	df = ""
 	if request.method == "POST":
+		lake_df = pd.read_csv("data_directory/lake_data.csv")
+		print()
+		geometry = ee.Geometry.Polygon(list(eval(lake_df[lake_df["Name"] == lake].iloc[0]["Coordinates "])))
 		start_date = request.form["start_date"]
 		end_date = request.form["end_date"]
 
@@ -70,4 +70,4 @@ def fetch(param):
 
 		except:
 			return redirect(url_for("errorPage"))
-	return render_template("value_display.html", param = param, df = df)
+	return render_template("value_display.html", param = param, lake = lake , df = df)
